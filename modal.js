95ed8630d17e148cb47ec61a6f7f94cad023f433
Axel GMX <agmx183@gmail.com>
2023-12-13 17:45:04 +0100

    Issue #2 : Implémenter entrées du formulaire - DONE

--- modal.js
+++ modal.js
@@ -29,77 +29,70 @@ function closeModal() {
 // close modal event
 btnCloseModal.addEventListener("click", closeModal);
 
+// Attend que le DOM soit entièrement chargé avant d'exécuter le script
 document.addEventListener("DOMContentLoaded", function () {
+  // Sélectionne le formulaire avec le nom "reserve"
   const form = document.forms.reserve;
 
+  // Ajoute un écouteur d'événements "submit" au formulaire
   form.addEventListener("submit", function (event) {
+    // Annule le comportement par défaut de soumission du formulaire
     event.preventDefault();
 
+    // Appelle la fonction de validation du formulaire
     if (validateForm()) {
       console.log("Formulaire valide. Données envoyées.");
     } else {
       console.log("Le formulaire n'est pas valide. Veuillez corriger les erreurs.");
+      // Les données du formulaire ne sont pas effacées ici, ce qui permet à l'utilisateur de les corriger sans perdre les saisies.
     }
   });
 
+  // Fonction de validation du formulaire
   function validateForm() {
-    const firstName = form.first.value;
-    const lastName = form.last.value;
-    const email = form.email.value;
-    const quantity = form.quantity.value;
-    const location = form.location.value;
-    const checkbox1 = form.checkbox1.checked;
+    // Récupération des valeurs des champs du formulaire
+    const firstName = form.first.value.trim(); //trim() enlève les espaces inutiles des valeurs des champs du formulaire
+    const lastName = form.last.value.trim();
+    const email = form.email.value.trim();
+    const quantity = form.quantity.value.trim();
+    const location = form.querySelector('input[name="location"]:checked');
+    const checkbox1 = form.checkbox1;
 
-    const isFirstNameValid = firstName.length >= 2;
-    const isLastNameValid = lastName.length >= 2;
+    // Vérification des conditions de validation
+    const isFirstNameValid = firstName.length >= 2 && !firstName == "";
+    const isLastNameValid = lastName.length >= 2 && !lastName == "";
     const isEmailValid = validateEmail(email);
-    const isQuantityValid = !isNaN(quantity) && quantity >= 0;
-    const isLocationValid = location !== undefined;
-    const isCheckbox1Valid = checkbox1;
+    const isQuantityValid = !isNaN(quantity) && quantity >= 0 && !quantity == "";
+    const isLocationValid = location !== null;
+    const isCheckbox1Valid = checkbox1.checked;
 
-    // Fonction pour afficher les messages d'erreur dans le HTML
-    function displayError(input, message) {
+    // Fonction pour afficher les messages d'erreur dans le DOM
+    function displayError(input, message, isValid) {
 
       const errorMessageElement = document.createElement("p");
       errorMessageElement.className = "error-message";
       errorMessageElement.textContent = message;
-      
-      const existingError = input.parentNode.querySelector(".error-message");
+
+      const existingMsgError = input.parentNode.querySelector(".error-message");
 
       // Vérifie si un message d'erreur n'existe pas déjà
-      if (!existingError) {
-        // Ajoute le nouveau message d'erreur sous l'élément d'entrée
-        input.parentNode.appendChild(errorMessageElement);
-      } else {
+      if (existingMsgError) {
         // Supprime l'élément du message d'erreur existant
-        input.parentNode.removeChild(existingError);
+        existingMsgError.remove();
+      }
+
+      if (!isValid) {
+        // Ajoute un message d'erreur sous l'élément d'entrée si condition invalide
         input.parentNode.appendChild(errorMessageElement);
       }
     }
 
-    if (!isFirstNameValid) {
-      displayError(form.first, "Le champ Prénom doit avoir au moins 2 caractères.");
-    } 
-
-    if (!isLastNameValid) {
-      displayError(form.last, "Le champ Nom doit avoir au moins 2 caractères.");
-    }
-
-    if (!isEmailValid) {
-      displayError(form.email, "Veuillez saisir une adresse e-mail valide.");
-    }
-
-    if (!isQuantityValid) {
-      displayError(form.quantity, "Veuillez saisir un nombre valide pour le nombre de concours.");
-    }
-
-    if (!isLocationValid) {
-      displayError(form.querySelector(".formData"), "Veuillez sélectionner une ville.");
-    }
-
-    if (!isCheckbox1Valid) {
-      displayError(form.checkbox1, "Veuillez accepter les conditions d'utilisation.");
-    }
+    displayError(form.first, "Le champ Prénom doit avoir au moins 2 caractères.", isFirstNameValid);
+    displayError(form.last, "Le champ Nom doit avoir au moins 2 caractères.", isLastNameValid);
+    displayError(form.email, "Veuillez saisir une adresse e-mail valide.", isEmailValid);
+    displayError(form.quantity, "Veuillez saisir un nombre valide pour le nombre de concours.", isQuantityValid);
+    displayError(form.querySelector(".location"), "Veuillez sélectionner une ville.", isLocationValid);
+    displayError(form.checkbox1, "Veuillez accepter les conditions d'utilisation.", isCheckbox1Valid);
 
     // Retourne true si toutes les conditions sont remplies, sinon false
     return isFirstNameValid && isLastNameValid && isEmailValid && isQuantityValid && isLocationValid && isCheckbox1Valid;
